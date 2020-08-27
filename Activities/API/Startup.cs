b215e4265712f7a7254024f3d5d116d4d4cded68
Darion Thompson <darionthompson2@gmail.com>
2020-08-27 14:29:13 -0400

    added ef core and created intial migaration

--- Activities/API/Startup.cs
+++ Activities/API/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api
 {
@@ -26,6 +28,10 @@ namespace Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<DataContext>(option => 
+            {
+                option.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
